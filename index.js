21c37e50aab36f073ec74a9353a4d8375c253c1d
Meow Roar <sareacct91@gmail.com>
2024-01-04 17:52:01 -0800

    Main js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const { writeFile } = require('fs');
+const getUserInput = require('./assets/script/userInput');
+const htmlTemplate = require('./assets/script/htmlString');
+
+(async () => {
+  writeFile(
+    'index.html',
+    htmlTemplate(await getUserInput()),
+    err => err ? console.log(err) : console.log('HTML file created')
+  );
+})();
